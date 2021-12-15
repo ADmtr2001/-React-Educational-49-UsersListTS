aad52a4f8dd2186f9c0e240a557d94cdb2ea66fc
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-15 19:27:25 +0200

    add List component and list items rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, {useState} from 'react';
+import './App.css';
+
+import List from "./components/List";
 
 interface IState {
   people: {
@@ -10,11 +13,19 @@ interface IState {
 }
 
 const App = () => {
-  const [people, setPeople] = useState<IState['people']>([]);
+  const [people, setPeople] = useState<IState['people']>([
+    {
+      name: 'Lebron James',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/LeBron_James_crop.jpg',
+      age: 36,
+      note: 'Allegeric to staying on the same team',
+    }
+  ]);
 
   return (
     <div className='App'>
       <h1>People Invited to my Party</h1>
+      <List people={people}/>
     </div>
   );
 };
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {FC} from 'react';
 
-const List = () => {
+interface IProps {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
+
+const List: FC<IProps> = ({people}) => {
+  const renderList = () : JSX.Element[] => {
+    return people.map((person) => {
+      return <li className='List'>
+        <div className='List-header'>
+          <img className='List-img' src={person.url}/>
+          <h2>{person.name}</h2>
+        </div>
+        <p>{person.age} years old</p>
+        <p className='List-note'>{person.note}</p>
+      </li>
+    })
+  }
+
   return (
     <div>
-      
+      {renderList()}
     </div>
   );
 };
